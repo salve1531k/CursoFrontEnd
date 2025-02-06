7274a8c50add49465e0c454bc0fd6274dedb00b8
salve1531K <mathuscostaribeiro@gmail.com>
2025-02-06 11:22:52 -0300

    06.02.2025

---
+++ introducaojs/script00.js
@@ -0,0 +1,32 @@
+const texto= "Olá Mundo!!!"
+var boasvindas = "Bem vindos, alunos de js"
+boasvindas = "Boas vindas alunos"
+
+
+console.log(texto);
+console.log(boasvindas)
+
+//Tipos de dados
+
+let string = " variavel de texto";//Sempre entre aspas simples ou asplas duplas
+let numero1 = 42;//numero simples
+let nuero2 = 10.65;//numero decimsl
+let numero3 = "2";//numero em texto
+let boolean = true;
+let boolean2 = false;
+let booleaan3 = "true";//texto
+let undefined;
+let nula = null;
+
+// let,var,const
+
+let resultado = true;
+resultado = false;
+//let resultado =false;
+
+var nota = 10;
+nota = 9
+var nota = 8;
+
+const fixo = 42;
+fixo =43;
---
+++ introducaojs/script01Operacoes.js
@@ -0,0 +1,38 @@
+//Tipos de operações em javascript
+
+let a = 10;
+let b = 3;
+
+console.log("Soma: ");
+console.log(a + b); //13
+console.log("Subtração: ");
+console.log(a - b) // 7
+console.log("multiplicação: ");
+console.log(a * b) //30
+console.log("Divisão: ");
+console.log(a / b)//3.33
+console.log("Resto da divisão: ");
+console(a % b); //1
+
+//Operadores de comparação
+
+// (> , >= , < , <= , == , ===)
+
+console.log("operadores Relacionados")
+console.log( 5 > 10);// False
+console.log(10 == 10);// True
+console.log(10 == "10");// False
+
+//Operadores Logícos
+
+let nota1 = 5;
+let nota2 = 8;
+
+// our (||)
+console.log(nota1>=7 || nota2>=7); //True
+
+// and (&&)
+console.log(nota1>=7 && nota2>=7); //False
+
+// not (!)
+console.log(!true); //False
